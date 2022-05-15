8d9099f21e571efa6ddfd0f6333757d3a34240e5
Julian Lopez <jlopezsa@gmail.com>
2022-05-14 21:47:13 -0500

    feat: Read only one data from real time data base

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,8 +1,21 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { getAccelerometerData } from '../utils/firebase';
 
 function HomePage() {
+  const [accelData, setAccelData] = useState(0);
+  useEffect(() => {
+    const accelerometerData = getAccelerometerData();
+    console.log('FROM DATA BASE RT:  ', JSON.stringify(accelerometerData));
+    console.log('FROM DATAASE RT:  ', accelerometerData.x);
+    setAccelData(accelerometerData.x);
+  }, [accelData]);
+
   return (
-    <div>HomePage</div>
+    <div>
+      <h1>HomePage</h1>
+      <h3>{accelData}</h3>
+    </div>
+
   );
 }
 
---
+++ src/utils/firebase.js
@@ -0,0 +1,40 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from 'firebase/app';
+import { getAnalytics } from 'firebase/analytics';
+import { getDatabase, ref, onValue } from 'firebase/database';
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: 'AIzaSyB-zVIq-CflSxXW9E50HhQtLKgheCAyO60',
+  authDomain: 'listeningsensors.firebaseapp.com',
+  databaseURL: 'https://listeningsensors-default-rtdb.firebaseio.com',
+  projectId: 'listeningsensors',
+  storageBucket: 'listeningsensors.appspot.com',
+  messagingSenderId: '634491630956',
+  appId: '1:634491630956:web:f6d37d278e93993c8daf49',
+  measurementId: 'G-E2B396DSM5',
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+const dbRT = getDatabase();
+
+const starCountRef = ref(dbRT, 'accelerometer/');
+
+function getAccelerometerData() {
+  let accelerometerData = {};
+  onValue(starCountRef, (snapshot) => {
+    const data = snapshot.val();
+    accelerometerData = data;
+    // updateStarCount(postElement, data);
+  });
+  return accelerometerData;
+}
+
+export {
+  app,
+  analytics,
+  getAccelerometerData,
+};
