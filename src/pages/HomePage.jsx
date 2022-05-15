45fb4a7db0c8cf789bfb632e3892a3c676cab0b1
Julian Lopez <jlopezsa@gmail.com>
2022-05-15 01:14:09 -0500

    feat: Print accelerometer data in a graphic

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,6 +1,55 @@
+/* eslint-disable */
 import React, { useEffect, useState } from 'react';
 import { ref, onValue } from 'firebase/database';
 import { dbRT } from '../utils/firebase';
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend,
+} from 'chart.js';
+import { Line } from 'react-chartjs-2';
+
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend
+);
+
+export const options = {
+  responsive: true,
+  plugins: {
+    legend: {
+      position: 'top',
+    },
+    title: {
+      display: true,
+      text: 'Accelerometer',
+    },
+  },
+  scales: {
+    xAxis: {
+      min: 0,
+      max: 1,
+    },
+    yAxis: {
+      min: -2,
+      max: 2,
+    }
+  }
+};
+
+const labels = [0, 5];
+
+
 
 function HomePage() {
   const [accelDataX, setAccelDataX] = useState(0);
@@ -16,6 +65,30 @@ function HomePage() {
       setAccelDataZ(data.z);
     });
   });
+
+  const data = {
+    labels,
+    datasets: [
+      {
+        label: 'x',
+        data: [accelDataX, accelDataX],
+        borderColor: 'rgb(255, 99, 132)',
+        backgroundColor: 'rgba(255, 99, 132, 0.5)',
+      },
+      {
+        label: 'y',
+        data: [accelDataY, accelDataY],
+        borderColor: 'rgb(53, 162, 235)',
+        backgroundColor: 'rgba(53, 162, 235, 0.5)',
+      },
+      {
+        label: 'z',
+        data: [accelDataZ, accelDataZ],
+        borderColor: 'rgb(53, 162, 2)',
+        backgroundColor: 'rgba(53, 162, 235, 0.1)',
+      },
+    ],
+  };
 
   return (
     <div>
@@ -35,6 +108,7 @@ function HomePage() {
         {' '}
         {accelDataZ}
       </h3>
+      <Line options={options} data={data} />
     </div>
 
   );
