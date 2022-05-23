e07422d64ffdb329b7a169cbd3da815ddda2135e
Julian Lopez <jlopezsa@gmail.com>
2022-05-23 01:33:00 -0500

    feat: Add sweetalert to show messages to user

--- src/components/molecules/FormCreateAccount.jsx
+++ src/components/molecules/FormCreateAccount.jsx
@@ -1,17 +1,23 @@
-/* eslint-disable */
 import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 import styled from 'styled-components';
-import { setDocument } from '../../utils/firebase';
 import {
   createUserWithEmailAndPassword,
-  FacebookAuthProvider ,
+  FacebookAuthProvider,
   getAuth,
   signInWithPopup,
 } from 'firebase/auth';
+import Swal from 'sweetalert2';
+import { setDocument } from '../../utils/firebase';
+import {
+  ROUTE_LOGIN,
+  ROUTE_DASHBOARD,
+} from '../../routes/routes';
 import { Button } from '../atomns/Button';
 import { Input } from '../atomns/Input';
-import iconFacebook from '../../figures/facebook_icon_square.svg';
+import { NavLinkStyledDark } from '../atomns/NavLinkStyledDark';
 import SocialNetwork from '../atomns/SocialNetworks';
+import iconFacebook from '../../figures/facebook_icon_square.svg';
 
 // Styled components
 const ContainerFormSignup = styled.div`
@@ -24,10 +30,10 @@ padding: 0px;
 const auth = getAuth();
 const provider = new FacebookAuthProvider();
 
-
 function FormCreateAccount() {
-  const [isAuthenticated, setIsAuthenticated] = useState(false);
-  const [user, setUser] = useState(null);
+  const navigate = useNavigate();
+  // const [isAuthenticated, setIsAuthenticated] = useState(false);
+  // const [user, setUser] = useState(null);
   const [userData, setUserData] = useState({
     userName: '',
     userEmail: '',
@@ -38,27 +44,67 @@ function FormCreateAccount() {
     setUserData({
       ...userData,
       [e.target.name]: e.target.value,
-    })
-  }
+    });
+  };
 
-  const handleCreateAccount = async () => {
+  const handleCreateAccount = async (e) => {
+    e.preventDefault();
     try {
-      const userCredential = createUserWithEmailAndPassword(auth, userData.userEmail, userData.userPassword)
-      setUser(userCredential.user);
+      // eslint-disable-next-line max-len
+      const userCredential = await createUserWithEmailAndPassword(auth, userData.userEmail, userData.userPassword);
+      console.log('FLAG-00: ', userCredential);
+
+      Swal.fire(
+        'Registro exitoso ',
+        'Ingresa tus datos para entrar a la plataforma!',
+        'success',
+      );
+
+      // setUser(userCredential.user);
+
+      await setDocument('users', {
+        id: userCredential.user.uid,
+        name: userData.userName,
+        email: userCredential.user.email,
+        photo: userCredential.user.photoURL,
+        role: 'user',
+      });
+
+      navigate(ROUTE_LOGIN);
     } catch (error) {
-      const errorCode = error.code;
-      const errorMessage = error.message;
-      console.log('ERROR: ', errorCode, errorMessage);
+      switch (error.code) {
+        case 'auth/email-already-in-use':
+          Swal.fire({
+            icon: 'error',
+            title: 'Algo salió mal',
+            text: 'Tu e-mail ya está registrado, intenta otro!',
+          });
+          break;
+        case 'auth/weak-password':
+          Swal.fire({
+            icon: 'error',
+            title: 'Algo salió mal',
+            text: 'Tu password debe tener como mínimo 6 caracteres!',
+          });
+          break;
+        default:
+          Swal.fire({
+            icon: 'error',
+            title: 'Algo salió mal',
+            text: 'Ingresa tus datos nuevamente',
+          });
+          break;
+      }
     }
-  }
+  };
 
   const handleLoginWithFacebook = async () => {
     try {
-      console.log('FLAG-01: ');
       const result = await signInWithPopup(auth, provider);
       // The signed-in user info.
-      const user = result.user;
-      setUser(user);
+      const { user } = result;
+      console.log('USERINFO: ', result);
+      // setUser(userInfo);
       await setDocument('users', {
         id: user.uid,
         name: user.displayName,
@@ -67,32 +113,43 @@ function FormCreateAccount() {
         role: 'user',
       });
 
+      Swal.fire(
+        'Registro exitoso ',
+        'Ya puedes acceder a los recursos de la plataforma!',
+        'success',
+      );
+      navigate(ROUTE_DASHBOARD);
       // This gives you a Facebook Access Token. You can use it to access the Facebook API.
       const credential = FacebookAuthProvider.credentialFromResult(result);
-      console.log("credential", credential)
-      const accessToken = credential.accessToken;
-      console.log("accessToken", accessToken)
+      console.log('credential', credential);
+      const { accessToken } = credential;
+      console.log('accessToken', accessToken);
     } catch (error) {
-      // console.log("error", error)
+      Swal.fire({
+        icon: 'error',
+        title: 'Algo salió mal',
+        text: 'No fue posible acceder a tu cuenta Facebook',
+      });
+      console.log('error', error);
     }
-  }
+  };
 
-  const handleLogin = async () => {
-
-  }
   return (
     <ContainerFormSignup>
       <form action="">
-        <Input onChange={handleChange} type='text' name='userName' placeholder="Ingrese nombre" />
-        <Input onChange={handleChange} type='email' name='userEmail' placeholder="Ingrese e-mail" />
-        <Input onChange={handleChange} type='password' name='userPassword' placeholder="Ingrese password" />
-        <h5>Ya tienes cuenta? Ingresa a la platafoma a traves del login </h5>
+        <Input onChange={handleChange} type="text" name="userName" placeholder="Ingrese nombre" />
+        <Input onChange={handleChange} type="email" name="userEmail" placeholder="Ingrese e-mail" />
+        <Input onChange={handleChange} type="password" name="userPassword" placeholder="Ingrese password" />
+        <h5>
+          Ya tienes cuenta?
+          <NavLinkStyledDark to={ROUTE_LOGIN}> Ingresa a traves del login! </NavLinkStyledDark>
+        </h5>
         <Button onClick={handleCreateAccount}>Crear cuenta</Button>
       </form>
-      <hr/>
+      <hr />
       <h5>También pedes ingresar usando tu red social</h5>
-      <Button onClick={handleLoginWithFacebook} >
-        <SocialNetwork imageSocial={iconFacebook}/>
+      <Button onClick={handleLoginWithFacebook}>
+        <SocialNetwork imageSocial={iconFacebook} />
       </Button>
     </ContainerFormSignup>
   );
