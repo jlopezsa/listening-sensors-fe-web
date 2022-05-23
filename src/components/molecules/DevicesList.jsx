e1d79b6dc092738c947208edd985f63023c3162b
Julian Lopez <jlopezsa@gmail.com>
2022-05-23 12:15:36 -0500

    feat: Create list devices component file

---
+++ src/components/molecules/DevicesList.jsx
@@ -0,0 +1,9 @@
+function DevicesList() {
+  return (
+    <h1>
+      Lista de dispositivos
+    </h1>
+  );
+}
+
+export default DevicesList;
--- src/pages/DashboardPage.jsx
+++ src/pages/DashboardPage.jsx
@@ -2,11 +2,13 @@ import React from 'react';
 import GyroscopeDashboard from '../components/organisms/GyroscopeDashboard';
 import AccelerometerDashboard from '../components/organisms/AccelerometerDashboard';
 import MagnetometerDashboard from '../components/organisms/MagnetometerDashboard';
+import DevicesList from '../components/molecules/DevicesList';
 
 function DashboardPage() {
   return (
     <div>
       DashboardPage
+      <DevicesList />
       <AccelerometerDashboard />
       <GyroscopeDashboard />
       <MagnetometerDashboard />
