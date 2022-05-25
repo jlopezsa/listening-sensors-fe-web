b90ffe728a0093d09a686ff74ef6a6c1186c4e56
Julian Lopez <jlopezsa@gmail.com>
2022-05-25 17:02:35 -0500

    feat: Hamuruer menu is open and close but without links

--- src/components/atomns/Menu.jsx
+++ src/components/atomns/Menu.jsx
@@ -1,9 +1,10 @@
 /* eslint-disale */
+import { useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { useNavigate } from 'react-router-dom';
 import styled from 'styled-components';
 import { getAuth, signOut } from 'firebase/auth';
-// import Hamburger from 'hamburger-react';
+import Hamburger from 'hamburger-react';
 import Swal from 'sweetalert2';
 import {
   ROUTE_HOME,
@@ -13,13 +14,29 @@ import {
 } from '../../routes/routes';
 import { NavLinkStyled } from './NavLinkStyled';
 import { saveUserLoged } from '../../store/actions';
+import { colors } from '../../css/globalStyles';
 
 const Nav = styled.nav`
   margin-right: 50px;
   margin-left: 50px;
-  display: true;
   @media (max-width: 690px) {
     display: none;
+}
+`;
+
+const NavBurguer = styled.nav`
+  margin-right: 50px;
+  margin-left: 50px;
+  @media (min-width: 690px) {
+    display: none;
+}
+`;
+
+const ContainerBurguer = styled.div`
+display: none;
+margin: 50px;
+@media (max-width: 690px) {
+  display: inline;
 }
 `;
 
@@ -28,28 +45,13 @@ const Ul = styled.ul`
   padding: 0px;
 `;
 
-/* const NavLinkStyledBurger = styled(NavLink)`
-background: black;
-  display: block;
-  position: absolute;
-  right: 0;
-  top: 0;
-  &:hover {
-  background-color: #ddd;
-  color: black;
-}
-`; */
-
-/* const II = styled.i`
-
-`; */
-
 const auth = getAuth();
 
 function Menu() {
   const userIsLoged = useSelector((state) => state.userLogin);
   const dispatch = useDispatch();
   const navigate = useNavigate();
+  const [isOpen, setIsOpen] = useState(false);
 
   const handleClick = async (e) => {
     e.preventDefault();
@@ -83,14 +85,50 @@ function Menu() {
     }
   };
 
-  /* const myFunction = () => {
-    const x = document.getElementById('myLinks');
-    if (x.style.display === 'block') {
-      x.style.display = 'none';
-    } else {
-      x.style.display = 'block';
-    }
-  }; */
+  const desplegarMenu = () => (
+    <NavBurguer>
+      <Ul>
+        <NavLinkStyled to={ROUTE_HOME}> Home </NavLinkStyled>
+        {!userIsLoged
+          ? null : <NavLinkStyled to={ROUTE_DASHBOARD}> Dashboard </NavLinkStyled> }
+        <NavLinkStyled to={ROUTE_HOME}> Team </NavLinkStyled>
+        <NavLinkStyled to={ROUTE_HOME}> Contact </NavLinkStyled>
+        {!userIsLoged
+          ? <NavLinkStyled to={ROUTE_SIGNUP}> Sign Up </NavLinkStyled> : null }
+        {!userIsLoged
+          ? <NavLinkStyled to={ROUTE_LOGIN}> Login </NavLinkStyled> : null }
+        {!userIsLoged
+          ? null
+          : <NavLinkStyled to={ROUTE_HOME} onClick={handleClick}> Logout </NavLinkStyled> }
+      </Ul>
+    </NavBurguer>
+  );
+
+  const cerrarMenu = () => {
+    <Nav>
+      <Ul>
+        <NavLinkStyled to={ROUTE_HOME}> Home </NavLinkStyled>
+        {!userIsLoged
+          ? null : <NavLinkStyled to={ROUTE_DASHBOARD}> Dashboard </NavLinkStyled> }
+        <NavLinkStyled to={ROUTE_HOME}> Team </NavLinkStyled>
+        <NavLinkStyled to={ROUTE_HOME}> Contact </NavLinkStyled>
+        {!userIsLoged
+          ? <NavLinkStyled to={ROUTE_SIGNUP}> Sign Up </NavLinkStyled> : null }
+        {!userIsLoged
+          ? <NavLinkStyled to={ROUTE_LOGIN}> Login </NavLinkStyled> : null }
+        {!userIsLoged
+          ? null
+          : <NavLinkStyled to={ROUTE_HOME} onClick={handleClick}> Logout </NavLinkStyled> }
+      </Ul>
+    </Nav>;
+  };
+
+  const showMenu = () => {
+    setIsOpen(true);
+  };
+  const ocultaMenu = () => {
+    setIsOpen(false);
+  };
 
   return (
     <div>
@@ -110,10 +148,20 @@ function Menu() {
             : <NavLinkStyled to={ROUTE_HOME} onClick={handleClick}> Logout </NavLinkStyled> }
         </Ul>
       </Nav>
-
-      {/* <Nav>
-        <Hamburger color="#4FD1C5" />
-      </Nav> */}
+      <ContainerBurguer>
+        <Hamburger
+          color={colors.backgroundOrange}
+          onToggle={(toggled) => {
+            if (toggled) {
+              showMenu();
+            } else {
+              ocultaMenu();
+              // close a menu
+            }
+          }}
+        />
+        {isOpen ? desplegarMenu() : cerrarMenu()}
+      </ContainerBurguer>
     </div>
   );
 }
