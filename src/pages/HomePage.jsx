6b48a85c1204ad7a260ce7e7c081424d42ad4256
Julian Lopez <jlopezsa@gmail.com>
2022-05-26 14:21:35 -0500

    fear: add architecture information

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -3,12 +3,20 @@ import slideItems from '../components/atomns/DataFigures';
 import SlidesImage from '../components/molecules/SlidesImage';
 import { colors } from '../css/globalStyles';
 import ContentCards from '../components/organisms/ContentCards';
+import InformationBigCard from '../components/molecules/InformationBigCard';
+import figAim from '../figures/listening-sensors.png';
+import figArchitecture from '../figures/architecture.png';
 // import { getImafFromFirebase } from '../utils/firebase';
 
 const ContainerSlider = styled.div`
 background-color: ${colors.backgroundDark};
 padding-top: 40px;
 padding-bottom: 40px;
+display: flex;
+justify-content: center;
+`;
+
+const ContainerInfo = styled.div`
 display: flex;
 justify-content: center;
 `;
@@ -27,7 +35,13 @@ function HomePage() {
       <ContainerSlider>
         <SlidesImage items={slideItems} />
       </ContainerSlider>
+      <ContainerInfo>
+        <InformationBigCard title="Lo que se hace como" content="Conexion de sensores" figure={figAim} />
+      </ContainerInfo>
       <ContentCards />
+      <ContainerInfo>
+        <InformationBigCard title="Arquitectura" content="Conexion de sensores" figure={figArchitecture} />
+      </ContainerInfo>
       {/* <img alt="" src={urlImage} /> */}
     </div>
   );
