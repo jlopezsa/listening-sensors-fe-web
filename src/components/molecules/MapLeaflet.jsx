ae6d53948c3e8d188ccdc848976c4fda5ca881c3
Julian Lopez <jlopezsa@gmail.com>
2022-06-12 23:45:43 -0500

    feat: Fix eslint sugestions

--- src/components/molecules/MapLeaflet.jsx
+++ src/components/molecules/MapLeaflet.jsx
@@ -1,14 +1,10 @@
-/* eslint-disable */
-import { useSelector } from "react-redux";
-import { useState, useEffect } from "react";
-import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet";
-import { Icon } from "leaflet";
-import markerIconPng from "../../css/images/marker-icon-2x.png";
-import "../../css/react-leaflet.css";
-import "../../css/leaflet.css";
-import "../../figures/react-leaflet-icon";
-
-// const position = [-6.45465465, 74.465465465];
+import { useSelector } from 'react-redux';
+import { MapContainer, Marker, Popup, TileLayer } from 'react-leaflet';
+import { Icon } from 'leaflet';
+import markerIconPng from '../../css/images/marker-icon-2x.png';
+import '../../css/react-leaflet.css';
+import '../../css/leaflet.css';
+import '../../figures/react-leaflet-icon';
 
 function MapLeaflet() {
   const positionSensor = useSelector((state) => state.sensorSetChoosed);
